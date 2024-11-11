308fd66e431abdccc258201f3302b7b448e713be
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-11-11 09:27:11 -0500

    serializing errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,24 @@
-import express from 'express'
-import { json } from 'body-parser'
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
+// src/index.ts
 
-const app = express()
-app.use(json())
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+const app = express();
+app.use(json());
 
-app.use(errorHandler)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// Ensure `errorHandler` is recognized as an error-handling middleware
+app.use(errorHandler as (err: Error, req: Request, res: Response, next: NextFunction) => void);
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')  
-})
+  console.log('Listening on port 3000');
+});
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,15 +1,22 @@
-import { Request, Response, NextFunction } from 'express'
-
+import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
-    err: Error, 
-    req: Request, 
-    res: Response, 
-    next: NextFunction
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    console.log('Somenthing went wrong', err)
+    if (err instanceof RequestValidationError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
 
-    res.status(400).send({
-        message: err.message
-    })
-}
+    if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
+};
