36c335d30a53137f62c7e1854706fb0d558d95da
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-11-11 17:36:49 -0500

    signup error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(json());
@@ -23,6 +24,19 @@ app.all('*', async (req, res) => {
 // Ensure `errorHandler` is recognized as an error-handling middleware
 app.use(errorHandler as (err: Error, req: Request, res: Response, next: NextFunction) => void);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDb')
+  } catch (err) {
+    console.error(err)
+  } 
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+}
+
+start()
+
+
